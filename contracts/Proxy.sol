a2268265079fa0af6bc83a378611f7d9d5be3384
ssolson <ssolson@sandia.gov>
2023-01-18 17:08:41 -0700

    Create a proxy contract in the contracts folder

---
+++ contracts/Proxy.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {UUPSProxy} from "@synthetixio/core-contracts/contracts/proxy/UUPSProxy.sol";
+
+contract Proxy is UUPSProxy {
+    // solhint-disable-next-line no-empty-blocks
+    constructor(address firstImplementation) UUPSProxy(firstImplementation) {}
+
+    function getImplementation() internal view virtual returns (address) {
+        return _proxyStore().implementation;
+    }
+}
